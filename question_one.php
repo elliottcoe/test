500d9dbbf42f25e6d85470fbdbdc89da21e121f5
elliottcoe <elliott@ontapmedia.co.uk>
2020-07-12 20:40:10 +0100

    question one complete, moving onto question two.

---
+++ question_one.php
@@ -0,0 +1,70 @@
+<?php
+
+// Original
+function get_data()
+{
+	$database = new database_connection_script_library;
+
+	$results = $database-get_recommended_products{"ALL");
+
+	$data = array();
+	foreach($results as $results)
+	{
+		$date[] = $results[0];
+	}
+
+	if($data_result)
+	{
+		return $data
+	)
+	else
+	{
+		return false;
+	}
+)
+
+
+// Fixed
+
+function get_data()
+{
+	// Need brackets on the library insantiation
+    $database = new database_connection_script_library();
+
+    // Missing arrow on method call
+    // curly bracket used instead of normal bracket
+	$results = $database->get_recommended_products("ALL");
+
+	// simpler array
+	$data = [];
+
+	// should use the singular of $results
+	foreach($results as $result)
+	{
+	    // $date should be $data
+	    // $results should be singular
+	    // and check array key exists
+
+	    if (array_key_exists($result, 0)) {
+	        $data[] = $results[0];
+	    }
+
+	}
+
+	// $data_result doesn't exist, assumed to be $data
+	if($data)
+	{
+
+	    // missing semi-colon
+		return $data;
+		// incorrect end bracket, should be curly bracket.
+	}
+	else
+	{
+		return false;
+	}
+	// Incorrectly used bracket, should be closing curly bracket
+}
+
+
+?>
